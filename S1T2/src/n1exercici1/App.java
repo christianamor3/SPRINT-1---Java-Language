68f3acac6fe50538e2a29035b2f3c08db1cb5fd2
Christian <amorlopezchristian@gmail.com>
2024-01-10 20:19:16 +0100

    Correccion 10012024

--- S1T2/src/n1exercici1/App.java
+++ S1T2/src/n1exercici1/App.java
@@ -4,45 +4,9 @@ public class App {
 
 	public static void main(String[] args) {
 		
-		Venda venda1 = new Venda();
-		Venda venda2 = new Venda();
+		Comprobaciones comprobacion1 = new Comprobaciones();
 		
-		/* Llamo al metodo sin añadir ningun objeto a la lista
-		para provocar la primera excepcion */
-		
-		try {
-			venda1.calcularTotal();
-		} catch (VendaBuidaException e) {
-			System.out.println(e.getMessage());
-		} catch (ArrayIndexOutOfBoundsException e) {
-			System.out.println(e.getMessage());
-		}
-		
-		Producte pr1 = new Producte("Movil", 200);
-		Producte pr2 = new Producte("Funda", 10);
-		Producte pr3 = new Producte("Casco", 75);
-		
-		/* Creo objetos de tipo Producte y los añado a la ArrayList
-		 , pero provocando una excepcion de ArrayIndexOutOfBounds al poner un =.
-		 */
-		venda1.getProductes().add(pr1);
-		venda2.getProductes().add(pr2);
-		venda1.getProductes().add(pr3);
-		
-		try {
-			venda1.calcularTotal();
-		} catch (VendaBuidaException e) {
-			System.out.println(e.getMessage());
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println(e.getMessage());
-		}
-		try {
-			venda2.calcularTotal();
-		} catch (VendaBuidaException e) {
-			System.out.println(e.getMessage());
-		} catch (IndexOutOfBoundsException e) {
-			System.out.println(e.getMessage());
-		}
+		comprobacion1.ejecucionPrograma();
 		
 
 	}
---
+++ S1T2/src/n1exercici1/Comprobaciones.java
@@ -0,0 +1,52 @@
+package n1exercici1;
+
+public class Comprobaciones {
+
+	public void ejecucionPrograma() {
+		
+		Venda venda1 = new Venda();
+		Venda venda2 = new Venda();
+		
+		/* Llamo al metodo sin añadir ningun objeto a la lista
+		para provocar la primera excepcion */
+		
+		try {
+			venda1.calcularTotal();
+		} catch (VendaBuidaException e) {
+			System.out.println(e.getMessage());
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+		/* Creo objetos de tipo Producte y los añado a la ArrayList
+		 , pero provocando una excepcion de ArrayIndexOutOfBounds al poner un =.
+		 */
+		
+		Producte pr1 = new Producte("Movil", 200);
+		Producte pr2 = new Producte("Funda", 10);
+		Producte pr3 = new Producte("Casco", 75);
+		
+		
+		venda1.getProductes().add(pr1);
+		venda2.getProductes().add(pr2);
+		venda1.getProductes().add(pr3);
+		
+		try {
+			venda1.calcularTotal();
+		} catch (VendaBuidaException e) {
+			System.out.println(e.getMessage());
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			venda2.calcularTotal();
+		} catch (VendaBuidaException e) {
+			System.out.println(e.getMessage());
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+
